cd9d99ef2746ba1ae7f9be4f3b847b5bf6257ebc
Felipe Andrade <andrade.adsf@gmail.com>
2023-01-02 15:27:10 -0300

    after vacation update

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 14:38:56 by feandrad          #+#    #+#             */
-/*   Updated: 2022/12/07 20:45:35 by feandrad         ###   ########.fr       */
+/*   Updated: 2023/01/02 15:26:44 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,13 +49,13 @@ int	check_flag(char flag, va_list arg)
 	else if (flag == 's')
 		return (ft_putstr_fd(va_arg(arg, char *), 1));
 	else if (flag == 'd' || flag == 'i')
-		return (ft_putstr_fd(ft_itoa(va_arg(arg, int)), 1);
+		return (ft_putstr_fd(ft_itoa(va_arg(arg, int)), 1));
     else if (flag == 'u')
-        return (ft_putstr_fd(ft_utoa(va_arg(arg, int))), 1);
+        return (ft_putstr_fd(ft_utoa(va_arg(arg, int)), 1));
     else if (flag == 'x' || flag == 'X')
-        return (ft_putstr_fd(ft_hextoa(va_arg(arg, int))), 1);
+        return (ft_putstr_fd(ft_hextoa(va_arg(arg, int)), 1));
     else if (flag == 'p')
-        return (search)
+        return (pointer_address);
     else
         return (ft_putchar(flag));
 }
